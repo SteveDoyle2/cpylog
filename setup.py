926dd0fbdb538f477a80f40b659cf5ff6c9b1063
SteveDoyle2 <mesheb82@gmail.com>
2019-05-23 08:38:18 -0700

    updating dependencies for CI

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ install_requires = []
 is_windows = 'nt' in os.name
 if is_travis and not is_windows:
     #install_requires.append('python-coveralls')
-    py_packages.append('codecov')
-    #py_packages.append('coverage')
+    install_requires.append('codecov')
+    #install_requires.append('coverage')
 
 
 # get package metadata
